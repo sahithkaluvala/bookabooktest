b73856add3c27d89ec2be7e5265fd85811781b8b
isayalimali <54920122+isayalimali@users.noreply.github.com>
2019-12-07 21:45:04 -0500

    Charmichanges updated for phase 3

--- js/login.js
+++ js/login.js
@@ -1,69 +1,40 @@
-/*function validate(){
- 
-   //window.alert("file");
-   //var user = document.forms["cform"]["username"].value;
-   var user= document.getElementById("username").value;
-   var pass=document.getElementById("password").value;
-   var sum=0;
-   let namepattern = /^[a-zA-Z]{2,12}$/;
-   //var sum =0;
-   if (user==''){
-   document.getElementById("dom").innerHTML="Please enter your name";
-   document.getElementById("dom").style.color="red";
-    //return false;
-  }
-  else if(!namepattern.test(user)){
-    document.getElementById("dom").innerHTML="Please enter Valid name";
-    document.getElementById("dom").style.color="red",'14s';
-     //return false;
-
-  }
-  
-  //local storage
-  
-}
-*/
-
 'use strict';
 
 $(document).ready(function() {
-  //alert(localStorage.getItem('name'));
+ 
   localStorage.removeItem('name');
   $('#add').click(function() {
     var textbox = $('#username');
-   // document.getElementById("username").value="";
+   
     var textbox1= $('#password');
-   // document.getElementById("password").value="";
-    
+    let namepattern = /^[a-zA-Z]{2,12}$/;
+    let passpattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$/;
    
     var task = textbox.val();
     var task1 = textbox1.val();
+   
+    /*checking conditions for validation */
+    if (task === ''|| (!namepattern.test(task)))  {
+      document.getElementById("dom").innerHTML="Please enter correct name ";
+      textbox.focus();
     
-    if (task1 === '')  {
-      document.getElementById("dom1").innerHTML="Please enter correct your password ";
-      //alert('Please enter your pass.');
-      textbox1.focus();
-    
-      } else if (task === '') {
-        document.getElementById("dom").innerHTML="Please enter your name";
-        //alert('Please enter your name.');
-        textbox.focus();
-      } else {
-
-      // add task to web storage
+      } else if (task1 === '' || (!passpattern.test(task1))) {
+        document.getElementById("dom1").innerHTML="Please enter correct password";
+        textbox1.focus();
+      }
+       else {
       let name = 'charmi';
       localStorage.setItem('name', name);
-
       var tasks = localStorage.C15tasks || ''; 
-      var t1 = tasks.concat('password: '+ task1 , '\n');
-     
+      var t1 = tasks.concat('password: '+ task1 , '\n'); 
       localStorage.C15tasks = t1.concat('name: '+ task, '\n'+'\n');
-
-      // clear task text box and re-display tasks
       textbox.val('');
-     // $('#text2').val(localStorage.C15tasks);
       textbox.focus();
     }
+
+    document.getElementById("username").value="";
+    document.getElementById("password").value=""; 
   });
+   
 });
 
--- js/signin.js
+++ js/signin.js
@@ -1,26 +1,63 @@
-function v(){
-    var firstname= document.getElementById("fname").value;
-    var lastname=document.getElementById("lname").value;
-    var mail=document.getElementById("email").value;
-    var pass=document.getElementById("psw").value;
-    var check=document.getElementById("check").value;
-    var sum =0;
-    let emailpattern=/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/
-    
-    if(firstname.length==0 || lastname.length==0 || mail.length==0 || pass.length==0) {
-       console.log(document.createElement("span"));
-    }
-    else if(!emailpattern.test(mail)){
-        alert("format is not correct..")
-    }
-    else if(!check.checked){
-        alert("checkbox is not selected...")
-    }
-    else if(pass.length<=6){
-        alert("password is small..")
-    }
-     else  {
-       alert("Form submmited")
-     }
+
+'use strict';
+
+$(document).ready(function() {
  
-}
+  localStorage.removeItem('name');
+  $('#add').click(function() {
+    var textbox = $('#fname');
+    var textbox1= $('#mail');
+    var textbox2=$('#psw')
+    var textbox3=$('#lname')
+
+    let namepattern = /^[a-zA-Z]{2,12}$/;
+    let passpattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$/;
+    let mailpattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+   
+   
+    var task = textbox.val();
+    var task1 = textbox1.val();
+    var task2 = textbox2.val();
+    var task3= textbox3.val();
+    
+    //checking the conditions
+
+    if (task === ''|| (!namepattern.test(task))) 
+     {
+      document.getElementById("dom").innerHTML="Please enter correct name ";
+      textbox.focus();
+    
+      }else if (task1 === '' || (!mailpattern.test(task1))) {
+        document.getElementById("dom1").innerHTML="Please enter correct email";
+        textbox1.focus();
+      } 
+       else if (task2 === '' || (!passpattern.test(task2))) {
+        document.getElementById("domp").innerHTML="Please enter correct password";
+        return false();
+      }
+      else if (task3 === '' ) {
+        document.getElementById("doml").innerHTML="Please enter last name";
+        return false();
+      } 
+     
+      else {
+      let name = 'charmi';
+      localStorage.setItem('name', name);
+      var tasks = localStorage.C15tasks || ''; 
+      var t1 = tasks.concat('mail: '+ task1 , '\n'); 
+      localStorage.C15tasks = t1.concat('name: '+ task, '\n'+'\n');
+      textbox.val('');
+      textbox.focus();
+    }
+
+    
+    document.getElementById("fname").value="";
+    document.getElementById("mail").value=""; 
+    document.getElementById("psw").value=""; 
+    document.getElementById("lname").value=""; 
+   
+    
+  });
+   
+});
+
